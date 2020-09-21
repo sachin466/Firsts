a669dec47a89cfad9f1a023ca81bd46f25278776
sachin466 <71390023+sachin466@users.noreply.github.com>
2020-09-21 16:08:34 +0530

    Update and rename fir.py to telegrambot.py

--- telegrambot.py
+++ telegrambot.py
@@ -1,15 +1,16 @@
 from telegram.ext import Updater,CommandHandler,MessageHandler,Filters
 from Adafruit_IO import Client,Data,Feed
+import os 
 def Turnoff(update, context):
   context.bot.send_message(chat_id=update.effective_chat.id, text="Light turned off")
-  context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://i.dlpng.com/static/png/7501809_preview.png')
+  context.bot.send_photo(chat_id=update.effective_chat.id,photo= 'https://png.pngtree.com/png-clipart/20190903/original/pngtree-c4d-simulation-physical-bulb-led-lighting-png-image_4450455.jpg')
   send_value(0)
 def Turnon(update, context):
   context.bot.send_message(chat_id=update.effective_chat.id, text="Light turned on")
-  context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://i.dlpng.com/static/png/507790_preview.png')
+  context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://png.pngtree.com/png-clipart/20200831/original/pngtree-glowing-white-led-bulb-png-image_5488088.jpg')
   send_value(1)
 def send_value(value):
-  feed = aio.feeds('lightc')
+  feed = aio.feeds('light')
   aio.send_data(feed.key,value)
 
 def input_message(update, context):
@@ -31,7 +32,7 @@ def start(update,context):
   context.bot.send_message(chat_id=update.effective_chat.id, text=start_message)
 ADAFRUIT_IO_USERNAME = os.getenv('ADAFRUIT_IO_USERNAME')
 ADAFRUIT_IO_KEY = os.getenv('ADAFRUIT_IO_KEY')
-TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv('TOKEN') 
 aio = Client(ADAFRUIT_IO_USERNAME,ADAFRUIT_IO_KEY)
 updater=Updater(TOKEN,use_context=True)
 dispatcher = updater.dispatcher
